d3649200f2e80542dc6c484f3339d6cf9d15a08c
Jack Matthews <jm5112356@gmail.com>
2022-05-10 23:43:37 -0400

    Initial EmbedFailFixup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
 import { Plugin, registerPlugin } from "enmity-api/plugins";
 import Manifest from "./manifest.json";
+import { getByProps, getModule } from "enmity-api/modules";
+import { create } from "enmity-api/patcher";
 
-const ExamplePlugin: Plugin = {
+const MessagesModule = getByProps("sendMessage");
+const UploadsModule = getByProps("uploadLocalFiles");
+
+const Patcher = create("embed-fail-fixup");
+
+const EmbedFailFixup: Plugin = {
   ...Manifest,
-  commands: [],
   patches: [],
 
   onStart() {
 
+    Patcher.before(MessagesModule, "sendMessage", (_, args, __) => {
+      args[1].content = args[1].content.replaceAll("media.discordapp.net", "cdn.discordapp.com")
+    });
+
+    Patcher.before(UploadsModule, "uploadLocalFiles", (_, args, __) => {
+      args[3].content = args[3].content.replaceAll("media.discordapp.net", "cdn.discordapp.com")
+    });
   },
 
   onStop() {
-
+    Patcher.unpatchAll()
   }
 }
 
-registerPlugin(ExamplePlugin);
+registerPlugin(EmbedFailFixup);
